ca21da9e03d9142c9a273a0d400cabfd588cbfd8
khan1203 <rk.rifat1203@gmail.com>
2025-05-31 00:37:19 +0600

    setup for vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
-python3.9 manage.py migrate
+python3.11 -m pip install -r requirements.txt
+python3.11 manage.py collectstatic --noinput
+python3.11 manage.py migrate
